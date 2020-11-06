95ee4ddcb5de7eb898d8d43d2da9bab3203633d3
CoyezFabien <coyez.fabien@gmail.com>
2020-11-06 18:14:21 +0100

    exercice2 jQuery

---
+++ assets/js/ex2.js
@@ -0,0 +1,8 @@
+$(function(){
+$('#text').show();
+});
+/*
+le signe $ permet de récuperer un element bien précis, on le détaille dans les parenthèses et les simple cote à l'intérieur de quel element on parle, ici on parle d'une ID pour preciser une ID dans le jQuery on met un # avec son nom.
+le . permet relie la méthode à ID
+.show permet de faire apparaitre des elements cacher à la base dans le CSS 
+*/ 
